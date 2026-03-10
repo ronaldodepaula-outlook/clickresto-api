d25580f83964f9066f72956080f0376a227472c0
ronaldo-de-paula <ronaldo_de_paula@hotmail.com>
2026-03-10 19:48:53 -0300

    Ajuste Perfil Usuarios2

--- app/Http/Controllers/Usuarios/PerfilController.php
+++ app/Http/Controllers/Usuarios/PerfilController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Usuarios;
 use App\Http\Controllers\BaseCrudController;
 use App\Models\Perfil;
 use Illuminate\Http\Request;
+use App\Services\AuthService;
 
 class PerfilController extends BaseCrudController
 {
+    public function __construct(private AuthService $authService) {}
+
     protected string $modelClass = Perfil::class;
 
     protected array $rules = [
@@ -16,6 +19,28 @@ class PerfilController extends BaseCrudController
     ];
 
     protected array $filterable = ['nome'];
+
+    public function index(Request $request)
+    {
+        $query = Perfil::query();
+
+        $user = $request->user();
+        if ($user && ! $this->authService->isAdminMaster($user)) {
+            $query->where('id', '!=', 1)
+                ->where('nome', '!=', 'admin_master');
+        }
+
+        foreach ($this->filterable as $field) {
+            if ($request->filled($field)) {
+                $query->where($field, $request->input($field));
+            }
+        }
+
+        $perPage = (int) $request->input('per_page', 15);
+        $perPage = max(1, min($perPage, 100));
+
+        return $query->paginate($perPage);
+    }
 
     public function update(Request $request, int $id)
     {
